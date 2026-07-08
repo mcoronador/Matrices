924611a0c4e237d1b2f4ee0c1801a494553129e1
milagroscoronado16@gmail.com <milagroscoronado16@gmail.com>
2026-07-08 09:57:30 -0500

    lectura del arrglo

--- omegaup.cpp
+++ omegaup.cpp
@@ -4,9 +4,12 @@ int main() {
 	int m;
 	int n;
 	cin>>n>>m;
+
+	int A[20][20];
 	
-	int [20][20];
-	
-	
-	
+	for (int i=0;i<m;i++){
+		for (int j= 0 ;j<n;j++){
+			cin>> A[i][j];
+		}
+		cout<<A[20][20];
 }
